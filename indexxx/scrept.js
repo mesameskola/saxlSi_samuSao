112acf3098838e3fe221c746dfe7e6d49f7786b3
mesameskola <skolamesame82@gmail.com>
2025-03-31 15:35:03 +0400

    full

--- indexxx/scrept.js
+++ indexxx/scrept.js
@@ -9,8 +9,8 @@ function btn(a) {
         screen.innerText = screen.innerText + a;
     }
 }
-function rts() {
-    screen.innerText = "";
+function rts(){
+    screen.innerText ="";
 }
 
 function del() {
@@ -18,20 +18,22 @@ function del() {
 }
 
 function operators(op) {
-    if (screen.innerText === "") {
-        screen.innerText = screen.innerText;
-    } else if (
-        screen.inerText.at(-1) === "+" ||
-        screen.inerText.at(-1) === "-" ||
-        screen.inerText.at(-1) === "*" ||
-        screen.inerText.at(-1) === "/"
+    if (screen.innerText === ""||
+        screen.innerText.at(-1) === "+" ||
+        screen.innerText.at(-1) === "-" ||
+        screen.innerText.at(-1) === "*" ||
+        screen.innerText.at(-1) === "/"
+       
     ) {
-        screen.innerText = screen.innerText.slice(0, -1) + op;
-    }
+        screen.innerText = screen.innerText.slice(0, -1)+op;
+    } 
     else {
         screen.innerText = screen.innerText + op;
     }
+
 }
+
+// console.log(screeen.innerText)
 
 function tolia() {
     screen.innerText = eval(screen.innerText);
